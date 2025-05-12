5688bb9bb6107b8dd5baf3e5bb2d99b1b9d9b337
riya <riyachauhan517@gmail.com>
2025-05-12 19:38:21 +0530

    folder restructuring and new project netflix gpt added

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='text-3xl font-bold text-green-600'>Hello World</div>
+    </div>
+  );
+}
+
+export default App;
